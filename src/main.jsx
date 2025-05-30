335b8ecf1a6b7697593db61dcafdad740ba0a31a
Xander-kira <xanderkira4life@gmail.com>
2025-05-30 13:57:05 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import App from './App.jsx'
 // import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </React.StrictMode>,
+
 
 )
